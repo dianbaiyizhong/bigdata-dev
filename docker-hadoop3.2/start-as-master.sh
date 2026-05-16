22591352da85b007bbde63cee6ee1ad76167dbf8
Borsalino <1585685035@qq.com>
2026-05-16 20:38:25 +0800

    完成hadoop3.2部署

--- docker-hadoop3.2/start-as-master.sh
+++ docker-hadoop3.2/start-as-master.sh
@@ -23,6 +23,6 @@ hadoop fs -chmod g+w /tmp
 hadoop fs -chmod g+w /user/hive/warehouse
 
 # 启动hive
-nohup hive --service metastore 2>&1 &
+# nohup hive --service metastore 2>&1 &
 nohup hive --service hiveserver2 -hiveconf hive.execution.engine=mr 2>&1 &
 nohup java -jar /usr/local/bigdata-dev-backend.jar 2>&1 &
